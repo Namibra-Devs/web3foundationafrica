3c789c9a0ea6b5bd51f63bdbf01f398b694c00e0
Ibrahim Sulemana <ibeevpersie@gmail.com>
2024-09-05 06:30:43 +0000

    Homepage done
    Finished with homepage
    Added FAQs, newsletter and footer
    Made some adjustments to some sections

--- main.js
+++ main.js
@@ -1,63 +1,86 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu");
+  const bars = document.querySelector(".bars");
+  const open = document.querySelector(".open");
+  const close = document.querySelector(".close");
 
-    // Responsive Navbar
-    const menu = document.querySelector('.menu');
-    const bars = document.querySelector('.bars');
-    const open = document.querySelector('.open');
-    const close = document.querySelector('.close');
-    
-    bars.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-        open.classList.toggle('hidden');
-        close.classList.toggle('hidden');
+  // close menu when clicking on a link
+  const links = document.querySelectorAll(".menu ul a");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.add("hidden");
+      open.classList.remove("hidden");
+      close.classList.add("hidden");
+    });
+  });
 
-        console.log('Clicked!')
-    
-        // close menu when clicking on a link
-        const links = document.querySelectorAll('.menu a');
-        links.forEach((link) => {
-            link.addEventListener('click', () => {
-                menu.classList.add('hidden');
-                open.classList.remove('hidden');
-                close.classList.add('hidden');
-            });
-        });
-    
-    
-    
-    
-        // close menu when clicking outside
-        window.addEventListener('click', (e) => {
-            if (e.target !== menu && e.target !== bars && e.target !== open && e.target !== close) {
-                menu.classList.add('hidden');
-                open.classList.remove('hidden');
-                close.classList.add('hidden');
-            }
-        });
+  // close menu when clicking outside
+  window.addEventListener("click", (e) => {
+    if (
+      e.target !== menu &&
+      e.target !== bars &&
+      e.target !== open &&
+      e.target !== close &&
+      !menu.contains(e.target)
+    ) {
+      menu.classList.add("hidden");
+      open.classList.remove("hidden");
+      close.classList.add("hidden");
+    }
+  });
+
+  bars.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    open.classList.toggle("hidden");
+    close.classList.toggle("hidden");
+
+    console.log("Clicked!");
+  });
+
+  //   Toggle for accordion
+  const accordions = document.querySelectorAll(".accordion");
+
+  // Toggle accordion content when clicking on a toggle element
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", () => {
+      const toggler = accordion.querySelector(".toggle");
+      const line = accordion.querySelector(".line");
+
+      toggler.classList.toggle("hidden");
+      line.classList.toggle("hidden");
     });
-    
-    
-    
-    
-    
-    
-    
-    // Scroll to top button
-    // const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
-    
-    // window.addEventListener('scroll', () => {
-    //     if (window.pageYOffset > 100) {
-    //         scrollToTopBtn.classList.add('show-scroll-btn');
-    //     } else {
-    //         scrollToTopBtn.classList.remove('show-scroll-btn');
-    //     }
-    // });
-    
-    // scrollToTopBtn.addEventListener('click', () => {
-    //     window.scrollTo({
-    //         top: 0,
-    //         behavior: 'smooth'
-        // });
-    // });
-    
-    });
+  });
+
+  //   Get current year for footer
+  const year = document.getElementById("year");
+  year.textContent = new Date().getFullYear();
+  // const accordions = document.querySelectorAll('.accordion');
+
+  // accordions.forEach(accordion => {
+  //   accordion.addEventListener("click", () => {
+  //     const toggler = accordion.querySelector(".toggle");
+  //     const line = accordion.querySelector(".line");
+
+  //     toggler.classList.toggle("hidden");
+  //     line.classList.toggle("hidden");
+  //   });
+  // });
+
+  // Scroll to top button
+  // const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
+
+  // window.addEventListener('scroll', () => {
+  //     if (window.pageYOffset > 100) {
+  //         scrollToTopBtn.classList.add('show-scroll-btn');
+  //     } else {
+  //         scrollToTopBtn.classList.remove('show-scroll-btn');
+  //     }
+  // });
+
+  // scrollToTopBtn.addEventListener('click', () => {
+  //     window.scrollTo({
+  //         top: 0,
+  //         behavior: 'smooth'
+  // });
+  // });
+});
